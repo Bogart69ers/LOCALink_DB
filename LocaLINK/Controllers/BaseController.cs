2c06d477c91e9c44fa54ec5920c6430dd49c95a8
Angldrie101 <89821301+Angldrie101@users.noreply.github.com>
2024-04-19 14:29:44 +0800

    Add project files.

---
+++ LocaLINK/Controllers/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using LocaLINK.Models;
+using LocaLINK.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocaLINK.Controllers
+{
+    public class BaseController : Controller
+    {
+        public String ErrorMessage;
+        public UserManager _userManager;
+        
+        public String Username { get { return User.Identity.Name; } }
+        public String UserId { get { return _userManager.GetUserByUsername(Username).userId; } }
+
+        public BaseController()
+        {
+            ErrorMessage = String.Empty;
+            _userManager = new UserManager();
+
+        }
+        public void IsUserLoggedSession()
+        {
+            UserLogged userLogged = new UserLogged();
+            if (User != null)
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    userLogged.UserAccount = _userManager.GetUserByUsername(User.Identity.Name);
+                    userLogged.UserInformation = _userManager.CreateOrRetrieve(userLogged.UserAccount.username, ref ErrorMessage);
+                }
+            }
+            Session["User"] = userLogged;
+        }
+    }
+}
---
+++ LocaLINK/Controllers/HomeController.cs
@@ -0,0 +1,143 @@
+﻿using LocaLINK.Contracts;
+using LocaLINK.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LocaLINK.Controllers
+{
+    [Authorize(Roles = "User, Worker")]
+    public class HomeController : BaseController
+    {
+        [AllowAnonymous]
+        // GET: Home
+        public ActionResult Index()
+        {
+            IsUserLoggedSession();
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(String ReturnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index");
+
+            ViewBag.Error = String.Empty;
+            ViewBag.ReturnUrl = ReturnUrl;
+
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(String username, String password, String ReturnUrl)
+        {
+            if (_userManager.Login(username, password, ref ErrorMessage) == ErrorCode.Success)
+            {
+                var user = _userManager.GetUserByUsername(username);
+
+                if (user.status != (Int32)status.Active)
+                {
+                    TempData["username"] = username;
+                    return RedirectToAction("Verify");
+                }
+                // 
+                FormsAuthentication.SetAuthCookie(username, false);
+                //
+                if (!String.IsNullOrEmpty(ReturnUrl))
+                    return Redirect(ReturnUrl);
+
+                switch (user.User_Role.rolename)
+                {
+                    case Constant.Role_User:
+                        //Ari ilisdi kung asa padung og si user maka login
+                        return RedirectToAction("Index");
+                    case Constant.Role_Worker:
+                        //Ari ilisdi kung asa padung og si worker maka login
+
+                        return RedirectToAction("Index");
+                    case Constant.Role_Admin:
+                        //Ari ilisdi kung asa padung og si admin maka login
+
+                        return RedirectToAction("Index");
+                    default:
+                        return RedirectToAction("Index");
+                }
+            }
+            ViewBag.Error = ErrorMessage;
+
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Verify()
+        {
+            if (String.IsNullOrEmpty(TempData["username"] as String))
+                return RedirectToAction("Login");
+
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Verify(String code, String username)
+        {
+            if (String.IsNullOrEmpty(username))
+                return RedirectToAction("Login");
+
+            TempData["username"] = username;
+
+            var user = _userManager.GetUserByUsername(username);
+
+            if (!user.code.Equals(code))
+            {
+                TempData["error"] = "Incorrect Code";
+                return View();
+            }
+
+            user.status = (Int32)status.Active;
+            _userManager.UpdateUser(user, ref ErrorMessage);
+
+            return RedirectToAction("Login");
+        }
+        [AllowAnonymous]
+        public ActionResult SignUp()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index");
+
+            ViewBag.Role = Utilities.ListRole;
+
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult SignUp(User_Account ua, String ConfirmPass)
+        {
+            if (!ua.password.Equals(ConfirmPass))
+            {
+                ModelState.AddModelError(String.Empty, "Password not match");
+                ViewBag.Role = Utilities.ListRole;
+                return View(ua);
+            }
+
+            if (_userManager.SignUp(ua, ref ErrorMessage) != ErrorCode.Success)
+            {
+                ModelState.AddModelError(String.Empty, ErrorMessage);
+
+                ViewBag.Role = Utilities.ListRole;
+                return View(ua);
+            }
+            TempData["username"] = ua.username;
+            return RedirectToAction("Verify");
+        }
+        [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
