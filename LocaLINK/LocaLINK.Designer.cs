3d4a8021a0953ce5eff9f307e13937e3aa6f3036
Angldrie101 <89821301+Angldrie101@users.noreply.github.com>
2024-04-21 19:06:01 +0800

    nwup

--- LocaLINK/LocaLINK.Designer.cs
+++ LocaLINK/LocaLINK.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Marvin\Documents\Visual Studio 2015\Projects\LocaLINK_IMDBSYS\LocaLINK\LocaLINK.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ANGEL DORIE\OneDrive\Documents\IMDBSYS\ENJELLOCALink-main (1)\ENJELLOCALink-main\LocaLINK\LocaLINK.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
